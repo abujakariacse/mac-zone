20da15d2493e3e448ad9a6270cc5616b049b5f5b
abujakariacse <abujakaria316@gmail.com>
2022-04-04 06:36:39 +0600

    notfound page done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,19 +9,19 @@ const Home = () => {
     const [customers] = useCustomer()
     return (
         <div className='container'>
-            <div className="row align-items-center">
-                <div className="col-lg-6 margin">
-                    <h1 className='size'>Your Next Phone</h1>
-                    <h1 className='size text-success'>Your best Phone</h1>
-                    <p>Motorcycle design varies greatly to suit a range of different purposes: long-distance travel, commuting, cruising, sport (including racing), and off-road riding.</p>
+            <div className="row align-items-center justify-content-between g-5">
+                <div className="col-lg-6">
+                    <h1 className='heading font-monospace text-secondary'>Your Next Phone</h1>
+                    <h1 className='heading text-success font-monospace'>Your best Phone</h1>
+                    <p className='font-monospace'>The phone which is powered by the new Apple A15 Bionic processor comes with a 6.7 inch Super Retina XDR OLED capacitive touchscreen and 2778 x 1284 pixels resolution.It features True Tone and a Wide color gamut. The display screen is protected by a Scratch-resistant ceramic glass, oleophobic coating, and features 120Hz, HDR10, Dolby Vision, 1000 nits (typ), and 1200 nits (peak).</p>
                     <button className='btn-success border-0 p-2 rounded'>Live Demo</button>
                 </div>
-                <div className="col-lg-6 margin">
+                <div className="col-lg-6">
                     <img className='img-fluid' src={Bike} alt="" />
                 </div>
             </div>
-            <div className='container mt-5'>
-                <h2 className='text-center text-success'>Customer Reviews - 3</h2>
+            <div className='container mt-3'>
+                <h2 className='text-center text-success'>Customer Reviews</h2>
             </div>
             <div className="container mt-5 grid">
                 {
@@ -32,7 +32,7 @@ const Home = () => {
                 }
             </div>
             <div className='d-flex justify-content-center mt-2 mb-5'>
-                <CustomLink to="/reviews" className="btn-success p-2 rounded">Show All Cart</CustomLink>
+                <CustomLink to="/reviews" className="btn-success p-2 rounded">More Reviews</CustomLink>
             </div>
         </div>
     );
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import error from "../../images/404-error.png"
+import './NotFound.css'
+// import error from "../../images/404-error.png"
 
 const NotFound = () => {
     return (
         <div className='container'>
-            <img className='img-fluid height' src={error} alt="" />
+            <h1 className='text-center mt-5 text-secondary fs-1'><i className='fa-solid fa-face-frown'></i></h1>
+            <h1 className='text-center font-monospace'>404 <span className='text-secondary'>error</span></h1>
+            <h1 className='text-center font-monospace'>Sorry, Page Not Found</h1>
+            <p className='text-center font-monospace mt-4 text-secondary'>The page you are looking for doesn't exist or an other error occurred.<br/>
+            Go back, or head over to mobilezone.com to choose a new direction</p>
         </div>
     );
 };
